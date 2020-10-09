9781a429868139ac9735fcad9288896dea4e907b
nithinj <nithinpottackal@gmail.com>
2020-10-08 20:14:57 -0400

    Signal unix

---
+++ signal.c
@@ -0,0 +1,31 @@
+#include<stdlib.h>
+#include<unistd.h>
+#include<stdio.h>
+#include<signal.h>
+#include <string.h>
+
+void
+handler(int num) {
+  int i = 1;
+  char buf[2] = "A";
+
+  while (1) {
+    write(STDOUT_FILENO, "handler\n", 9);
+    write(STDOUT_FILENO, buf, sizeof(buf));
+    sleep(i++);
+    buf[0]++;
+  }
+}
+
+
+int
+main()
+{
+  signal(SIGINT, handler);
+  signal(SIGTERM, handler);
+  while (1) {
+    printf("main pgm is executing; pid: %d\n", getpid());
+    sleep(1);
+  }
+}
+    
